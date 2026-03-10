aa06727d38588836e8fb2ca5927f9e5ef3d9f184
Daniel Altorf <daniel.altorf@zoolt.com>
2026-03-10 14:15:20 +0100

    Another attempt

--- terraform/synthetics/main.tf
+++ terraform/synthetics/main.tf
@@ -1,41 +1,32 @@
+data "grafana_synthetic_monitoring_probes" "main" {}
 resource "grafana_synthetic_monitoring_check" "Synthetics_BrowserCheck_login" {
-  job     = "Browser:Login"
-  target  = "login"
-  enabled = true
-  probes  = [1]
-  labels = {
-    check_type = "browser"
-  }
+  job       = "Synthetics:BrowserCheck"
+  target    = "login"
+  enabled   = true
+  probes    = [data.grafana_synthetic_monitoring_probes.main.probes.London]
+  labels    = {}
   frequency = 300000
   timeout   = 60000
+
   settings {
     browser {
       script = file("${path.module}/../../scripts/browser.js")
     }
   }
-
-  lifecycle {
-    create_before_destroy = false
-  }
 }
 
-resource "grafana_synthetic_monitoring_check" "Http_GetPizza" {
-  job     = "Http:GetPizza"
-  target  = "http"
-  enabled = true
-  probes  = [1]
-  labels = {
-    check_type = "http"
-  }
+resource "grafana_synthetic_monitoring_check" "Synthetics_HttpCheck" {
+  job       = "Synthetics:HttpCheck"
+  target    = "http"
+  enabled   = true
+  probes    = [data.grafana_synthetic_monitoring_probes.main.probes.Frankfurt]
+  labels    = {}
   frequency = 300000
   timeout   = 60000
+
   settings {
     browser {
       script = file("${path.module}/../../scripts/http.js")
     }
-  }
-
-  lifecycle {
-    create_before_destroy = false
   }
 }
